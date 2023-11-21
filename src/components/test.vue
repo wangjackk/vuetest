a39b16e4056ed3fd1d8163d239de1bba719ed7ac
wangjinke <60803248+wangjackk@users.noreply.github.com>
2023-11-21 23:31:56 +0800

    Initial commit

---
+++ src/components/test.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-table :data="tableData"
+            :cell-style="{ textAlign: 'center' }"
+            :header-cell-style="{ 'text-align': 'center' }">
+    <el-table-column prop="id" label="ID" width="80"></el-table-column>
+    <el-table-column prop="name" label="Name" width="180"></el-table-column>
+    <el-table-column prop="usage" label="Usage" width="100"></el-table-column>
+    <el-table-column prop="work" label="Work" width="180"></el-table-column>
+
+  </el-table>
+</template>
+<!---->
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import axios from 'axios';
+import { ElTable, ElTableColumn } from 'element-plus';
+
+const tableData = ref([]);
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/pcs');
+    tableData.value = response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    // 处理错误情况，例如显示错误消息
+  }
+};
+
+let intervalId;
+
+onMounted(() => {
+  fetchData(); // 首次加载时获取数据
+  intervalId = setInterval(fetchData, 1000); // 每5秒刷新一次数据
+});
+
+onUnmounted(() => {
+  clearInterval(intervalId); // 组件卸载时清除定时器
+});
+</script>
+
+<style>
+/* 在这里添加您的样式 */
+</style>
